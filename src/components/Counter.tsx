301a3da1dd9f6b25072a2a00f23add63265acdb1
kiendang <kien.dang@cgw.com.au>
2020-09-02 17:38:23 +1000

    Add countup and scroll visible sensor

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { IconProp, SizeProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styled from 'styled-components'
+import CountUp from 'react-countup'
+import VisibilitySensor from 'react-visibility-sensor'
 
 interface Props {
   className?: string
@@ -28,7 +30,17 @@ export class Counter extends React.Component<Props> {
             <FontAwesomeIcon icon={props.icon} size={props.iconSize} />
           </Box>
           <div className="details">
-            <h3 className="mb-0 mt-0 number"><em className="count">{props.count}</em></h3>
+            <h3 className="mb-0 mt-0 number">
+              <em className="count">
+                <CountUp end={props.count}>
+                  {({ countUpRef, start }) => (
+                    <VisibilitySensor onChange={start} delayedCall>
+                      <span ref={countUpRef} />
+                    </VisibilitySensor>
+                  )}
+                </CountUp>
+              </em>
+            </h3>
             <p className="mb-0">{props.description}</p>
           </div>
         </div>
